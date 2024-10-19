93dc6d7451b350e58a3f7e11845dd13905e0b546
bucz <119478207+Buczjc@users.noreply.github.com>
2024-10-20 01:41:59 +0800

    Add files via upload

---
+++ La Lune French Bistro Prototype Website/scrollAnimation.js
@@ -0,0 +1,55 @@
+ScrollReveal({
+  reset: true,
+  distance: "30px",
+  duration: 1500,
+  delay: 200,
+});
+
+ScrollReveal().reveal(".main_section h1", {
+  delay: 100,
+  origin: "top",
+});
+ScrollReveal().reveal(".main_section p", {
+  delay: 100,
+  origin: "bottom",
+});
+ScrollReveal().reveal(".main_section_anchor", {
+  delay: 200,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".img_container", {
+  delay: 100,
+  origin: "left",
+});
+ScrollReveal().reveal(".about_section_content", {
+  delay: 100,
+  origin: "right",
+});
+
+ScrollReveal().reveal(".menu_content", {
+  delay: 100,
+  origin: "left",
+});
+ScrollReveal().reveal(".menu_items", {
+  delay: 200,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".review_section_header", {
+  delay: 100,
+  origin: "top",
+});
+ScrollReveal().reveal(".review_section_container", {
+  delay: 200,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".footer_content_container h1", {
+  delay: 100,
+  origin: "top",
+});
+ScrollReveal().reveal(".footer_contacts", {
+  delay: 200,
+  origin: "left",
+});
